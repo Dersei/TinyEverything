d3edf8f42678f7389d68b0adfaf956b6e9985ac0
Dominik Andrzejczak <dersei0248@gmail.com>
2019-05-25 23:18:04 +0200

    Texturing

--- TinyEverything/TinyRaycasterProject/TinyRaycaster.cs
+++ TinyEverything/TinyRaycasterProject/TinyRaycaster.cs
@@ -34,6 +34,21 @@ namespace TinyEverything.TinyRaycasterProject
             return true;
         }
 
+        List<uint> TextureColumn(List<uint> img, int texsize, int ntextures, int texid, int texcoord, int column_height)
+        {
+            int img_w = texsize * ntextures;
+            int img_h = texsize;
+
+            List<uint> column = Enumerable.Repeat(0u, column_height).ToList();
+            for (int y = 0; y < column_height; y++)
+            {
+                int pix_x = texid * texsize + texcoord;
+                int pix_y = (y * texsize) / column_height;
+                column[y] = img[pix_x + pix_y * img_w];
+            }
+            return column;
+        }
+
         private uint PackColor(byte r, byte g, byte b, byte a = 255)
         {
             return (((uint)a << 24) + ((uint)b << 16) + ((uint)g << 8) + r);
@@ -72,9 +87,9 @@ namespace TinyEverything.TinyRaycasterProject
         "0     0  1110000" +
         "0     3        0" +
         "0   10000      0" +
-        "0   0   11100  0" +
-        "0   0   0      0" +
-        "0   0   1  00000" +
+        "0   3   11100  0" +
+        "5   4   0      0" +
+        "5   4   1  00000" +
         "0       1      0" +
         "2       1      0" +
         "0       0      0" +
@@ -86,6 +101,8 @@ namespace TinyEverything.TinyRaycasterProject
         private readonly string _directoryName = $"dir-{DateTime.Now:yyyy-dd-M--HH-mm-ss.fff}";
         public void Run()
         {
+            LoadTexture("Resources/walltext.png", out var walltext, out var walltext_size, out var walltext_count);
+
             Directory.CreateDirectory(_directoryName);
             const int width = 1024; // image width
             const int height = 512; // image height
@@ -116,9 +133,9 @@ namespace TinyEverything.TinyRaycasterProject
                         if (_map[i + j * MapWidth] == ' ') continue; // skip empty spaces
                         var rectX = i * rectW;
                         var rectY = j * rectH;
-                        var icolor = _map[i + j * MapWidth] - '0';
-                        Debug.Assert(icolor < ncolors);
-                        DrawRectangle(framebuffer, width, height, rectX, rectY, rectW, rectH, colors[icolor]);
+                        var texid = _map[i + j * MapWidth] - '0';
+                        Debug.Assert(texid < walltext_count);
+                        DrawRectangle(framebuffer, width, height, rectX, rectY, rectW, rectH, walltext[texid * walltext_size]);
                     }
                 }
 
@@ -137,12 +154,29 @@ namespace TinyEverything.TinyRaycasterProject
 
                         if (_map[(int)(cx) + (int)(cy) * MapWidth] != ' ')
                         {
-                            int icolor = _map[(int)cx + (int)cy * MapWidth] - '0';
-                            Debug.Assert(icolor < ncolors);
+                            int texid = _map[(int)cx + (int)cy * MapWidth] - '0';
+                            Debug.Assert(texid < walltext_count);
                             // our ray touches a wall, so draw the vertical column to create an illusion of 3D
                             int column_height = (int)(height / (t * MathF.Cos(angle - player_a)));
-                            DrawRectangle(framebuffer, width, height, width / 2 + i, height / 2 - column_height / 2, 1,
-                                column_height, colors[icolor]);
+                            float hitx = cx - MathF.Floor(cx + 0.5f); // hitx and hity contain (signed) fractional parts of cx and cy,
+                            float hity = cy - MathF.Floor(cy + 0.5f); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
+                            int x_texcoord = (int)(hitx * walltext_size);
+                            if (MathF.Abs(hity) > MathF.Abs(hitx))
+                            {
+                                x_texcoord = (int)(hity * walltext_size);
+                            }
+
+                            if (x_texcoord < 0) x_texcoord += walltext_size; // do not forget x_texcoord can be negative, fix that
+                            Debug.Assert(x_texcoord >= 0 && x_texcoord < (int)walltext_size);
+
+                            List<uint> column = TextureColumn(walltext, walltext_size, walltext_count, texid, x_texcoord, column_height);
+                            pix_x = width / 2 + i;
+                            for (var j = 0; j < column_height; j++)
+                            {
+                                pix_y = j + height / 2 - column_height / 2;
+                                if (pix_y < 0 || pix_y >= (int)height) continue;
+                                framebuffer[pix_x + pix_y * width] = column[j];
+                            }
                             break;
                         }
                     }
